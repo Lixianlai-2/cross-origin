73c2f8ebdf0d174844d1481c16b4d4da5f39967c
MarkLee <1643565650@qq.com>
2022-02-06 14:58:56 +0800

    封装JSONPP

--- hacker.com/public/hacker.js
+++ hacker.com/public/hacker.js
@@ -11,21 +11,58 @@
 
 // ---------------------------------------
 // 通过JSONP访问
-const random = Math.random();
+
 // const random = Math.random();
-console.log(random);
 
-window[random] = (data) => {
-  console.log(data);
+// // 如果这里是数字的话，必须用[]
+// window[random] = (data) => {
+//   console.log(data);
+// };
+
+// const script = document.createElement("script");
+// // 通过js得到数据
+// script.src = `http://lixianlai:8888/friends.js?functionName=${random}`;
+
+// // 得到script之后将其删掉
+// script.onload = () => {
+//   script.remove();
+// };
+
+// document.body.appendChild(script);
+
+// ---------------------------------------
+// 封装JSONP
+const JSONP = function (url) {
+  return new Promise((resolve, reject) => {
+    const random = Math.random();
+
+    // 如果这里是数字的话，必须用[]，必须跟friends.js中的一致，那里写得是window[]
+    window[random] = (data) => {
+      console.log(data);
+
+      // 这个resolve有什么作用？
+      resolve(data);
+    };
+
+    const script = document.createElement("script");
+    // 通过js得到数据
+    script.src = `${url}?functionName=${random}`;
+
+    // 得到script之后将其删掉
+    script.onload = () => {
+      script.remove();
+    };
+
+    script.onerror = () => {
+      reject();
+    };
+
+    document.body.appendChild(script);
+  });
 };
 
-const script = document.createElement("script");
-// 通过js得到数据
-script.src = `http://lixianlai:8888/friends.js?functionName=${random}`;
+// JSONP("http://lixianlai:8888/friends.js").then((data) => {
+//   console.log(data);
+// });
 
-// 得到script之后将其删掉
-script.onload = () => {
-  script.remove();
-};
-
-document.body.appendChild(script);
+JSONP("http://lixianlai:8888/friends.js");
